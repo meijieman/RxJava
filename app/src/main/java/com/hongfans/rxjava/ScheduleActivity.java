b8002a4104ca377daef58bf9fdc75baedd0f3908
meijieman <1558667079@qq.com>
2018-02-07 14:06:25 +0800

    添加切换线程的示例

--- app/src/main/java/com/hongfans/rxjava/ScheduleActivity.java
+++ app/src/main/java/com/hongfans/rxjava/ScheduleActivity.java
@@ -2,13 +2,20 @@ package com.hongfans.rxjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import java.util.concurrent.TimeUnit;
 
+import rx.Notification;
+import rx.Observable;
 import rx.Scheduler;
+import rx.Subscriber;
 import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class ScheduleActivity extends AppCompatActivity implements View.OnClickListener{
@@ -38,12 +45,12 @@ public class ScheduleActivity extends AppCompatActivity implements View.OnClickL
 
                     @Override
                     public void call(){
-                        count++;
-                        LogUtil.i("aaaaaaaaaaa ");
-
                         if(count == 5){
                             worker.unsubscribe();
+                            return;
                         }
+                        count++;
+                        LogUtil.i("aaaaaaaaaaa ");
                         worker.schedule(this);
                     }
                 });
@@ -87,7 +94,8 @@ public class ScheduleActivity extends AppCompatActivity implements View.OnClickL
             }
             break;
             case R.id.btn_4:
-
+                // 线程切换
+                method1();
                 break;
             case R.id.btn_5:
 
@@ -99,5 +107,61 @@ public class ScheduleActivity extends AppCompatActivity implements View.OnClickL
 
                 break;
         }
+    }
+
+    private void method1() {
+        Observable
+                .create(new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> subscriber) {
+                        Log.i("tag_rx", "create s " + Thread.currentThread().getName());
+                        subscriber.onNext("hello");
+                        Log.i("tag_rx", "create e " + Thread.currentThread().getName());
+                        subscriber.onCompleted();
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .doOnSubscribe(new Action0() {
+                    @Override
+                    public void call() {
+                        Log.i("tag_rx", "doOnSubscribe " + Thread.currentThread().getName());
+                    }
+                })
+                .doOnUnsubscribe(new Action0() {
+                    @Override
+                    public void call() {
+                        Log.i("tag_rx", "doOnUnsubscribe " + Thread.currentThread().getName());
+                    }
+                })
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .observeOn(Schedulers.io())
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String s) {
+                        Log.i("tag_rx", "map " + Thread.currentThread().getName());
+
+                        return s + " world ";
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.i("tag_rx", "doOnNext " + Thread.currentThread().getName());
+                    }
+                })
+                .doOnEach(new Action1<Notification<? super String>>() {
+                    @Override
+                    public void call(Notification<? super String> notification) {
+                        Log.i("tag_rx", "doOnEach " + notification.getKind() + ", " + notification.getValue() + ", " + Thread.currentThread().getName());
+                    }
+                })
+                .unsubscribeOn(Schedulers.io())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.i("tag_rx", "subscribe " + Thread.currentThread().getName());
+                    }
+                });
     }
 }
